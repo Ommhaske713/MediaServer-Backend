d2fe25bb594998596c72cf914fd7a1dc985e08da
om mhaske <ommhaske1224@gmail.com>
2024-09-05 19:25:59 +0530

    Added the accountDetails and change password controller

--- src/models/subscription.model.js
+++ src/models/subscription.model.js
@@ -6,20 +6,21 @@ import { User } from "./user.model";
 const subscriptionSchema = new Schema({
     subscriber:{
 
-        type:Schema.Types.ObjectId,     //  one who is subscribing channel
+        type:Schema.Types.ObjectId,     //  one who is subscribing channel 
         ref:User                       
 
     },
     channel:{
 
         type:Schema.Types.ObjectId,     // one to whom 'subscriber' is subscribing 
-        ref:User 
+        ref:User
 
     }
 
 },{timestamps:true});
 
 export const Subscription = mongoose.model("Subscription",subscriptionSchema);
+
 
 
 // import mongoose from 'mongoose';
