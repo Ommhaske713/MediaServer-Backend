bfcd96d135c17eb0d196f9d955246f797be806b4
om mhaske <ommhaske1224@gmail.com>
2024-09-05 18:21:28 +0530

    Added subscription model

---
+++ src/models/subscription.model.js
@@ -0,0 +1,27 @@
+import mongoose,{Schema} from "mongoose";
+import { User } from "./user.model";
+
+
+
+const subscriptionSchema = new Schema({
+    subscriber:{
+
+        type:Schema.Types.ObjectId,     //  one who is subscribing channel
+        ref:User                       
+
+    },
+    channel:{
+
+        type:Schema.Types.ObjectId,     // one to whom 'subscriber' is subscribing 
+        ref:User 
+
+    }
+
+},{timestamps:true});
+
+export const Subscription = mongoose.model("Subscription",subscriptionSchema);
+
+
+// import mongoose from 'mongoose';
+// const subTodoSchema = new mongoose.Schema({}, { timestamps: true });
+// export const SubTodo = mongoose.model('SubTodo', subTodoSchema);
